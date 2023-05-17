4d209c6ae2e23b7bee52b984dbf83b8952fbd32f
haidev99bk <haidev99bk@gmail.com>
2023-05-17 17:19:59 +0700

    done

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
+/*
+
+             1  
+            /  \
+           2    3
+          / \   / \
+         4   5  6  7 
+         
+         
+              1` 
+            /  \
+           2    3`
+          / \   / \
+         4   5  6  7 
+         
+   // Imagine that node 3 is changed. We would want to rerender node 3 and it's parents/ancestors.
+   // Given a changed node, return the list of nodes that we need to rerender.   
+     
+   // Examples:  
+   findDirtyNodes(root, 4) -> [1,2,4]
+   findDirtyNodes(root, 3) -> [1,3]
+   
+   
+   Given a binary tree, write a function that takes in a node value returns true if the value is found in the tree. else return false.
+   
+   findNode(root, 4) -> true
+   findNode(root, 8) -> false
+   
+             1  
+            /  \
+           2    3
+          / \   / \
+         4   5  6  7 
+   {
+     node: 1
+     left: {
+         node: 2 {
+             left: {
+                 node: 4
+                 
+             }
+             right: {
+                 node: 5
+             }
+         }
+     }
+
+*/
+
 const input = {
   node: 1,
   left: {
@@ -15,26 +64,35 @@ const input = {
 };
 
 const fun = (input, findNumber) => {
-  let x = false;
-  let left = [];
-  let right = [];
+  let hasResult = false;
+  let y = [];
 
-  const _fnc = (input, findNumber) => {
-    if (input["left"]) {
-      _fnc(input["left"], findNumber);
-    }
-    if (input["right"]) {
-      _fnc(input["right"], findNumber);
-    }
+  const saver = [];
 
+  const _fnc = (input, findNumber, side, saver) => {
+    let _saver = [...saver, input.node];
     if (Number(input["node"]) === findNumber) {
-      x = true;
+      y = saver;
+      return (hasResult = true);
+    }
+
+    if (input[side]) {
+      _fnc(input[side], findNumber, "left", _saver);
+      _fnc(input[side], findNumber, "right", _saver);
+    }
+    if (input[side]?.node) {
+      saver.push(input[side].node);
     }
   };
 
-  _fnc(input, findNumber);
+  _fnc(input, findNumber, "right", saver);
 
-  console.log(x);
+  if (!hasResult) {
+    console.log("run here");
+    _fnc(input, findNumber, "left", saver);
+  }
+
+  console.log(y);
 };
 
-fun(input, 898895);
+fun(input, 4);
